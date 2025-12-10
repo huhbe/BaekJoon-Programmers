4a07136328b235f4e6385a6b108da7a45c7429be
huhbe <142882186+huhbe@users.noreply.github.com>
2025-12-10 17:00:23 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 1.29 ms, Memory: 81.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Solution {
+    public String solution(String my_string) {
+        char[] c=my_string.toLowerCase().toCharArray();
+        Arrays.sort(c);
+        String answer =new String(c);
+        return answer;
+    }
+}
